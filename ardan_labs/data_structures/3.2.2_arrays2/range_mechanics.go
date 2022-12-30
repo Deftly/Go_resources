ae5dfa959620fafa8e0c57fbac88a8629350b2c7
Deftly <deftkz@gmail.com>
2022-12-30 13:49:15 -0800

    value vs pointer semantics using range

--- ardan_labs/data_structures/3.2.2_arrays2/range_mechanics.go
+++ ardan_labs/data_structures/3.2.2_arrays2/range_mechanics.go
@@ -10,4 +10,19 @@ func main() {
   for i, v := range friends {
     fmt.Printf("Value[%s]\tAddress[%p] IndexAddr[%p]\n", v, &v, &friends[i])
   }
+
+  /* Using the value semantic form of the for range.
+  The reason Betty is printed instead of Jack is because this for range
+  isn't iterating over the friends array but rather its own copy, so any
+  changes to friends is irrelevant in this iteration */
+  fmt.Printf("Bfr[%s] : ", friends[1])
+  for i, v := range friends {
+    friends[1] = "Jack"
+
+    if i == 1 {
+      fmt.Printf("v[%s]\n", v)
+    }
+  }
+
+  fmt.Printf("v[%s]\n", friends[1]) // Jack
 }
