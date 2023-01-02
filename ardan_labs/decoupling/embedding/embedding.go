f4992648178e451dc8a48535b0b2e57fae9473c0
Deftly <deftkz@gmail.com>
2023-01-02 13:55:08 -0800

    embedding in go

---
+++ ardan_labs/decoupling/embedding/embedding.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// user defines a user in the program
+type user struct {
+  name string
+  email string
+}
+
+// notify is a method that notifies users of different events.
+func (u *user) notify() {
+  fmt.Printf("Sending user email to %s<%s>\n", u.name, u.email)
+}
+
+// admin represents an admin user with privileges.
+type admin struct {
+  user // Embedded type
+  level string
+}
+
+func main() {
+  // Create an admin user.
+  ad := admin{
+    user: user{
+      name: "john smith",
+      email: "john@email.com",
+    },
+    level: "super",
+  }
+
+  // We can access the inner type's method directly.
+  ad.user.notify()
+  // The inner type's method is promoted.
+  ad.notify()
+}
